c0d1000834cd9c8d8355ca64f422503592a0d79b
Josue <jquilon-barrios@myseneca.ca>
2019-09-18 22:23:11 -0400

    Removed left 0 for single digits

--- 86_64/lab3/loop.s
+++ 86_64/lab3/loop.s
@@ -7,8 +7,10 @@ max = 35                        /* loop exits when the index hits this number (l
 _start:
     mov     $start,%r15         /* loop index */
 
-loop:
+loopmd:
 
+	cmp    $10,%r15			/* see if loop index is less than 10 */
+	jl     loopsg
 	mov    $10,%r13			/* moved 10 to r13 */
 	mov    %r15,%rax		/* moved r15 to rax */
 	mov    $48,%r14			/* moved 48 to r14*/
@@ -27,10 +29,23 @@ loop:
         mov    $1,%rdi                  /* file descriptor stdout */
         mov    $1,%rax                  /* syscall sys_write */
         syscall
+	jmp    check
 
+loopsg:
+
+	mov    $48,%r14			/* moved 48 to r14*/
+	add    %r15,%r14		/* added r15 to r14 */
+	mov    %r14b,msg+6		/* added number after 'Loop' */
+        mov    $len,%rdx		/* message length */
+        mov    $msg,%rsi		/* message location */
+        mov    $1,%rdi			/* file descriptor stdout */
+        mov    $1,%rax			/* syscall sys_write */
+        syscall
+	
+check:	
 	inc     %r15			/* increment index */
 	cmp     $max,%r15		/* see if we're done */
-	jne     loop			/* loop if we're not */
+	jne     loopmd			/* loop if we're not */
 
 	mov     $0,%rdi			/* exit status */
 	mov     $60,%rax		/* syscall sys_exit */
