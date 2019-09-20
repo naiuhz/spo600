157d0fedd30d4d2bf8f8c441954fdc06a67a7f9e
miguel <mroncancio19@gmail.com>
2019-09-20 13:08:23 -0400

    update comments

--- arm64/lab3/loop.s
+++ arm64/lab3/loop.s
@@ -5,20 +5,22 @@ start = 0                       /* starting value for the loop index; note that 
 max = 10                        /* loop exits when the index hits this number (loop condition is i<max) */
 
 _start:
-    mov     r20,start         /* loop index */
+    	mov    x20,start         /* loop index */
 
 loop:
 
-	add    r19,r20,48                        /* added r20+48 to r19 */
-	ldrb   msg+6,r19                      /* added number after 'Loop' */
-	mov    x2,len                      /* message length */
-	adr    x1,msg                       /* message location */
-	mov    x0,1                         /* file descriptor stdout */
+	add    x19, x20,48 			/* add 48 to x20 and store in x19*/
+	adr    x21, msg                        /* putting the address of msg into x21 */
+	strb   w19, [x21,6]                      /* store one byte from w19 to x21 plus an offset of 6 */
+	mov    x2,len                      /* message length syscall args*/
+	adr    x1,msg                       /* message location syscall args*/
+	mov    x0,1                         /* file descriptor stdout syscall args*/
 	mov    x8,64                        /* syscall sys_write */
 	svc    0
 
-	add     r20,r20,1                /* increment index */
-	cmp     max,r20           /* see if we're done */
+	add     x20,x20,1                /* increment index */
+	mov	x22,max
+	cmp     x20,x22           /* see if we're done */
 	b.ne    loop                /* loop if we're not */
 
 	mov     x0,0             /* exit status */
